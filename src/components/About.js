0a12921f512367ec3768bb20a1526c68a8629329
Ryan <salvat36@gmail.com>
2023-04-30 05:55:37 -0500

    passing all labs

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+
+  function HasBio() {
+    return props.bio ? <p>{props.bio}</p> : null
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <HasBio />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
+
 
 export default About;
